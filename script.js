059aadf752b5a5c2eb9f76dabc24a0afd7746a6b
KRLH Blupine <krlh.blupine@proton.me>
2023-10-30 15:59:37 -0500

    reset to the point where the button was working

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
 const btn = document.querySelector(".btn"); 
-const cbtn = document.querySelector(".close");
-let open = 0;
 
 // Listen for click event 
-btn.onclick = function (c) { 
-    if(open = 0){
+btn.onclick = function (e) { 
+
     // Create span element 
-        let ripple = document.createElement("span"); 
+    let ripple = document.createElement("span"); 
 
     // Add ripple class to span 
-        ripple.classList.add("ripple"); 
+    ripple.classList.add("ripple"); 
 
     // Add span to the button 
-        this.appendChild(ripple); 
+    this.appendChild(ripple); 
 
     // Get position of X 
-        let x = e.clientX - e.target.offsetLeft; 
+    let x = e.clientX - e.target.offsetLeft; 
 
     // Get position of Y 
-        let y = e.clientY - e.target.offsetTop; 
+    let y = e.clientY - e.target.offsetTop; 
 
     // Position the span element 
-        ripple.style.left = `${x}px`; 
-        ripple.style.top = `${y}px`; 
+    ripple.style.left = `${x}px`; 
+    ripple.style.top = `${y}px`; 
 
-        let open = 1;
-    }
-    else {
-        return;
-    };
+    // Remove span after 0.3s 
+    setTimeout(() => { 
+        ripple.remove(); 
+    }, 5000); 
+
+
+
 }; 
-close.onclick = function (c) {
-    if(open = 1){
-        ripple.remove
-    }
-}
